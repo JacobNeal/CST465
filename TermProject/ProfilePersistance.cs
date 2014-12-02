831965ae5dd3a38bb7c4355e35ce804b119c1146
Jacob Neal <jacob.neal@oit.edu>
2014-12-01 23:49:29 -0800

    Updated Project for Assignment 5
    Updated the project for assignment 5. Still have to add a LoadProfile
    function.

---
+++ TermProject/ProfilePersistance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+namespace TermProject
+{
+    public class ProfilePersistance
+    {
+        public void SaveProfile(ProfileData profile)
+        {
+            string queryString = "UserProfile_InsertUpdate";
+            using (SqlConnection connection = new SqlConnection("Server=aura.students.cset.oit.edu;database=JacobNeal;user id=JacobNeal; password=jacob;"))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@UserID", Membership.GetUser().ProviderUserKey.ToString());
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+
+                    }
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
+            }
+        }
+
+        public void LoadProfile(ProfileData profile)
+        {
+
+        }
+    }
+}
